7fbdd2f174262aa60e6f79e5bf79cfa20ca593b0
Eric Kim <bosung90@gmail.com>
2016-05-20 21:38:16 -0700

    use different config

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+// var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+// var app = express();
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// // start server on the specified port and binding host
+// app.listen(appEnv.port, '0.0.0.0', function() {
+//   // print a message when the server starts listening
+//   console.log("server starting on " + appEnv.url);
+// });
+
 var ghost = require('ghost');
 ghost().then(function (ghostServer) {
-    ghostServer.start();
+	ghostServer.config.url = appEnv.url;
+	ghostServer.config.server.host = '0.0.0.0';
+	ghostServer.config.server.port = process.env.PORT || appEnv.port || 8080;
+  ghostServer.start();
 });
