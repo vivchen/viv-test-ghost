5ff5a631e12e34fb25a6f5c7743555eeaf3851d4
Eric Kim <bosung90@gmail.com>
2016-05-21 02:13:08 -0700

    Adding back config.js and use that to initialize ghost server

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
+var path = require('path');
 var ghost = require('ghost');
-ghost().then(function (ghostServer) {
-	ghostServer.config.url = appEnv.url;
-	ghostServer.config.server.host = '0.0.0.0';
-	ghostServer.config.server.port = process.env.PORT || appEnv.port || 8080;
+ghost({
+	config: path.join(__dirname, 'config.js')
+}).then(function (ghostServer) {
   ghostServer.start();
 });
