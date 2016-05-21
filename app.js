8fa0e915a2ecd6bcd13d1e497cc8c8639f34ad91
Eric Kim <bosung90@gmail.com>
2016-05-20 22:07:14 -0700

    code cleanup

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-// var express = require('express');
-
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
-// create a new express server
-// var app = express();
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-
-// // start server on the specified port and binding host
-// app.listen(appEnv.port, '0.0.0.0', function() {
-//   // print a message when the server starts listening
-//   console.log("server starting on " + appEnv.url);
-// });
 
 var ghost = require('ghost');
 ghost().then(function (ghostServer) {
